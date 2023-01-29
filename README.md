246c4912e962ec1268ac908bcfe1e4e2abdb2fa3
lico <lionel.conforto@gmail.com>
2023-01-29 08:29:12 +0100

    Updating the CDN

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # lico.github.io
 CDN for JavaScript, Images and CSS
+Created on 2023-01-29
--- pushGit.bat
+++ pushGit.bat
@@ -1,5 +1,3 @@
-REM git add --all
-git commit --all -m 2022.12-R02
-git push --set-upstream https://github.com/lico/sandbox-h2-spring-boot-desk.git master
-git tag -a 2022.12-R02 -m 2022.12-R02
-git push https://github.com/lico/sandbox-h2-spring-boot-desk.git 2022.12-R02
+git add --all
+git commit --all -m "Updating the CDN"
+git push --set-upstream https://github.com/lico/lico.github.io master
